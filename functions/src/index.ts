4f85237471039fc6a1dc814d5218dac71b30ad46
Ji-hoon Lee <anteater1056@gmail.com>
2022-04-30 19:20:40 +0900

    회원 가입/회원 삭제 시 firesotre와의 연동 function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
@@ -6,4 +7,33 @@ import * as functions from "firebase-functions";
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
+});
+
+export const createUserStore = functions.auth.user().onCreate((user) => {
+  let app;
+  if (!admin.apps.length) app = admin.initializeApp();
+  else app = admin.app();
+
+  const userObject = {
+    email: user.email,
+    registeredAt: user.metadata.creationTime,
+  };
+
+  return admin
+    .firestore(app)
+    .doc("users/" + user.uid)
+    .set(userObject);
+});
+
+export const deleteUserStore = functions.auth.user().onDelete((user) => {
+  let app;
+  if (!admin.apps.length) app = admin.initializeApp();
+  else app = admin.app();
+
+  // TBD 참조형 필드를 삭제하는 로직 개발
+
+  return admin
+    .firestore(app)
+    .doc("users/" + user.uid)
+    .delete();
 });
